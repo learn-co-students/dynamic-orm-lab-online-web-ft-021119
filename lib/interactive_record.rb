1b3cac50ea5c700c354e98f2fb66884749e06c50
Joan Lyness <joanrigdon@gmail.com>
2019-03-24 02:20:26 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -11,10 +11,10 @@ class InteractiveRecord
     self.class.table_name
   end
 
+
   def self.column_names
     DB[:conn].results_as_hash = true
     column_names = []
-
     sql = "PRAGMA table_info('#{table_name}')"
     table_info = DB[:conn].execute(sql)
 
@@ -25,12 +25,45 @@ class InteractiveRecord
     column_names.compact # => ["id", "name", "grade"]
   end
 
+  def col_names_for_insert
+    self.class.column_names.delete_if{|col| col == "id"}.join(", ")
+  end
+
+  def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{self.send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
+
   def initialize(options={})
     options.each do |k,v|
       self.send("#{k}=", v)
     end
   end
 
+  # def save
+  #   question_marks_for_insert = values_for_insert.split.map {|val| val = "?"}.join(", ")
+  #   sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{question_marks_for_insert})"
+  #   sql = sql.split.compact.join(" ")
+  #
+  #   DB[:conn].execute(sql, "#{values_for_insert.split[1..-1].join}")
+  #   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  # end
+
+  def save
+    sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+    DB[:conn].execute(sql)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{table_name} WHERE self.name = ?"
+    result =  DB[:conn].execute(sql, name)[0]
+    binding.pry
+  end
 
 
 
