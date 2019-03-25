62a41f76ae9eb0af0073b8dac9665e1f24b60e08
Christoph Gruener <christophgruener@me.com>
2019-03-25 15:26:57 +0000

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -46,29 +46,48 @@ class InteractiveRecord
     col_names.compact.join(', ')
   end
 
+
   def new_col_names
     self.col_names_for_insert.split(", ").map{|i| "'"+i+"'"}.join
   end
 
   def values_for_insert
-    # new = self.col_names_for_insert.split(", ").map{|i| "'"+i+"'"}.join(", ")
-    sql = <<-SQL
-      SELECT '#{new_col_names}'
-      FROM '#{table_name_for_insert}'
-    SQL
-    binding.pry
-
-    new = DB[:conn].execute(sql)
-
-    # new = self.col_names_for_insert.split(", ").map{|i| "'"+i+"'"}.join(", ")
-    # col_names_for_insert.split(", ").map{|i| "'"+i+"'"}.join(", ")
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
   end
 
+  def save
+    sql = <<-SQL
+    INSERT INTO #{table_name_for_insert} (#{col_names_for_insert})
+    VALUES (#{values_for_insert})
+    SQL
 
+    DB[:conn].execute(sql)
 
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]
+  end
 
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
 
-
+  def self.find_by(attribute)
+    column = attribute.keys
+    data = nil
+    if attribute.values == Integer
+      data = attribute.values
+    else
+      data = "'#{attribute.values}'"
+    end
+    sql = <<-SQL
+    SELECT * FROM #{self.table_name} WHERE #{column[0]} = #{data}
+    SQL
+    DB[:conn].execute(sql)
+  end
 
 
 end
