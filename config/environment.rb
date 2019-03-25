1e4f873cc3f48c56d8233b2e86bfba9235cf90bf
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-25 02:12:20 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
-require 'sqlite3'
+require 'sqlite3''~>1.3.6'
 require 'pry'
+# A sample Gemfile
+# source "https://rubygems.org"
+#
+# gem 'pry'
+# gem 'sqlite3', '~>1.3.6'
 
 DB = {:conn => SQLite3::Database.new("db/students.db")}
 DB[:conn].execute("DROP TABLE IF EXISTS students")
