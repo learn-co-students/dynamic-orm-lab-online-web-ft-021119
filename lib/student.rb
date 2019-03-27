4f2eab7343f8a6fad78ba1c7ae646129c8495556
Lizzie <lchan217@gmail.com>
2019-03-27 09:45:09 -0500

    solved table_name

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,13 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  # def self.hello
+  #   puts "hello"
+  # end
+
 
 end
