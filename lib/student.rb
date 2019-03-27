61c9a70c46023a73ca20f37f791cbb63423a108b
Lizzie <lchan217@gmail.com>
2019-03-27 10:50:34 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,13 +3,7 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  def self.table_name
-    self.to_s.downcase.pluralize
-  end
-
-  # def self.hello
-  #   puts "hello"
-  # end
-
-
+  self.column_names.each do |col_name|
+       attr_accessor col_name.to_sym
+ end
 end
